67f2a11c937b597ed3c3e562916a0407c049c8ed
mallikarjun <vkmallikarjun@gmail.com>
2022-08-27 21:04:33 +0530

    adding a curl file

--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 
-LIST_OF_APPS="git docker.io openjdk-11-jre awscli"
+LIST_OF_APPS="git docker.io openjdk-11-jre"
 
 echo "$LIST_OF_APPS"
 
 sudo apt-get update
 sudo apt-get install -y $LIST_OF_APPS
+
+echo curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 echo "The git version is" `git --version`
 echo "The docker version is" `docker --version`
